fa8fb7d5cc6e91b2b10ce931c1ad14d1159b3867
amd64 <kkir039@gmail.com>
2022-03-13 21:17:54 +0300

    Add OnePlus 9 Pro support

--- anykernel.sh
+++ anykernel.sh
@@ -1,28 +1,33 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
+# blu_spark changes by eng.stk
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=### DragKernel for Samsung Galaxy Tab S7+###
+kernel.string=### blu_spark kernel for OnePlus 9/Pro ###
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=gts7xl
-device.name2=SM-T975
-device.name3=
-device.name4=
-device.name5=
-supported.versions=
+device.name1=OnePlus9Pro
+device.name2=lemonadep
+device.name3=OnePlus9ProTMO
+device.name4=lemonadept
+device.name5=OnePlus9
+device.name6=lemonade
+device.name7=OnePlus9TMO
+device.name8=lemonadet
+device.name9=OnePlus9VZW
+device.name10=lemonadev
+supported.versions=11
 '; } # end properties
 
-## shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=0;
+# shell variables
+block=boot;
+is_slot_device=1;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -32,29 +37,37 @@ patch_vbmeta_flag=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 
-## AnyKernel install
+
+## Trim partitions
+$bin/busybox fstrim -v /data;
+
+
+## AnyKernel boot install
 dump_boot;
-patch_cmdline "androidboot.selinux=permissive" "";
-patch_cmdline "is_power_efficient" "";
+
+# remove old root patch avoidance hack
+patch_cmdline "skip_override" "";
+
 write_boot;
+## end boot install
 
 
 # shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+block=vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 
 ## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+split_boot;
 
-#flash_boot;
+# remove old root patch avoidance hack
+patch_cmdline "skip_override" "";
+
+flash_boot;
 ## end vendor_boot install
-
